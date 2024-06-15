bf814f866a605f84efbb6a172ee2f439f8e17a4d
Martin Batek <martin.batek@gmail.com>
2024-06-15 12:35:00 +0100

    retireving shuffled training samples from s3

--- scripts/s3_data_retrieval/retrieve_samples_from_s3.py
+++ scripts/s3_data_retrieval/retrieve_samples_from_s3.py
@@ -1,32 +1,80 @@
 # %%
 # Import necessary libraries
 import pandas as pd
+import boto3
 import sys
 
-# Retrieve the sample size argument
-n = int(sys.argv[1])
+# Set sample size
+n = sys.argv[1]
 
 # %%
-# Read in kdd training from S3
-kdd12_data = pd.read_csv(
-    's3://mlds-final-project-bucket/kdd12/train/training/training.txt',
-    delimiter='\t',
-    nrows = n,
-    header=None,
-    names=['Click','Impression','DisplayURL','AdID','AdvertiserID','Depth','Position','QueryID','KeywordID','TitleID','DescriptionID','UserID']
-    )
-kdd12_data.to_csv('./data/kdd12/kdd12_training.csv', index=False)
+# Read in kdd training data from S3
+bucket = 'mlds-final-project-bucket'
+prefix = 'kdd12/train/training_shuffled/'
+
+## Create an S3 client
+s3_client = boto3.client('s3')
+
+## List objects within a S3 bucket prefix and read to pandas for the first n rows
+response = s3_client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+kdd12_data = pd.DataFrame()
+rows = n
+if 'Contents' in response:
+    keys = [obj['Key'] for obj in response['Contents']][1:]
+    for key in keys:
+        if rows == 0:
+            break
+        obj = s3_client.get_object(Bucket=bucket, Key=key)
+        df = pd.read_csv(
+            obj['Body'],
+            nrows = rows,
+            header=0,
+            names=['Click','Impression','DisplayURL','AdID','AdvertiserID','Depth','Position','QueryID','KeywordID','TitleID','DescriptionID','UserID']
+            )
+        kdd12_data = pd.concat([kdd12_data, df])
+        rows -= df.shape[0]
+else:
+    print("Folder is empty.")
+
+## Save the data to a csv file
+kdd12_data.to_csv('../../data/kdd12/kdd12_training.csv', index=False)
 
 # %%
 # Read in avazu training data from S3
-avazu_training_data = pd.read_csv(
-    's3://mlds-final-project-bucket/avazu/train/train.csv',
-    nrows = n
-    )
-avazu_training_data.to_csv('./data/avazu/avazu_train.csv', index=False)
+bucket = 'mlds-final-project-bucket'
+prefix = 'avazu/train_shuffled/'
+
+## Create an S3 client
+s3_client = boto3.client('s3')
+
+## List objects within a S3 bucket prefix and read to pandas for the first n rows
+response = s3_client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+avazu_training_data = pd.DataFrame()
+rows = n
+if 'Contents' in response:
+    keys = [obj['Key'] for obj in response['Contents']][1:]
+    for key in keys:
+        if rows == 0:
+            break
+        obj = s3_client.get_object(Bucket=bucket, Key=key)
+        df = pd.read_csv(
+            obj['Body'],
+            nrows = rows
+            )
+        avazu_training_data = pd.concat([avazu_training_data, df])
+        rows -= df.shape[0]
+else:
+    print("Folder is empty.")
+
+## Save the data to a csv file
+avazu_training_data.to_csv('../../data/avazu/avazu_train.csv', index=False)
 
 # %%
-# Create column name list as per the README file
+# Read in criteo training data from S3
+bucket = 'mlds-final-project-bucket'
+prefix = 'dac/train_shuffled/'
+
+## Create column name list as per the README file
 criteo_names = ['click']
 for i in range(1, 14):
     criteo_names.append('int_{0}'.format(i))
@@ -34,14 +82,31 @@ for i in range(1, 14):
 for i in range(1, 27):
     criteo_names.append('cat_{0}'.format(i))
 
-# Read in criteo training data from S3
-criteo_training_data = pd.read_csv(
-    's3://mlds-final-project-bucket/dac/train/train.txt',
-    nrows = n,
-    sep='\t',
-    header=None,
-    names=criteo_names
-    )
-criteo_training_data.to_csv('./data/criteo/criteo_train.csv', index=False)
+## Create an S3 client
+s3_client = boto3.client('s3')
+
+## List objects within a S3 bucket prefix and read to pandas for the first n rows
+response = s3_client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+criteo_training_data = pd.DataFrame()
+rows = n
+if 'Contents' in response:
+    keys = [obj['Key'] for obj in response['Contents']][1:]
+    for key in keys:
+        if rows == 0:
+            break
+        obj = s3_client.get_object(Bucket=bucket, Key=key)
+        df = pd.read_csv(
+            obj['Body'],
+            nrows = rows,
+            header=0,
+            names=criteo_names
+            )
+        criteo_training_data = pd.concat([criteo_training_data, df])
+        rows -= df.shape[0]
+else:
+    print("Folder is empty.")
+
+## Save the data to a csv file
+criteo_training_data.to_csv('../../data/criteo/criteo_train.csv', index=False)
 
 
